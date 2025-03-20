5fb29ebba936d322bcfcddd4d189ed457271899f
MrCroatia <petar.vlcek@yahoo.com>
2025-03-20 15:47:27 +0100

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
